28595fb0e2dc58b1df91ed26e1db5f783231bd0d
xetayac <307517616+xetayac@users.noreply.github.com>
2026-09-08 18:19:48 +0300

    Untested removed

--- src/weather_api/api.py
+++ src/weather_api/api.py
@@ -3,17 +3,6 @@ import requests
 WEATHER_API_KEY = "f574b5a2ae174c37946140519260709"
 
 
-def get_weather(city: str) -> dict[str, any]:
-    params = {
-        "q": city,
-        "key": WEATHER_API_KEY
-    }
-
-    data = None
-
-    response = requests.get("https://api.weatherapi.com/v1/current.json", params=params)
-    return response.json()["current"]
-
 def get_timezone(city: str) -> dict[str, any]:
     params = {
         "q": city,
