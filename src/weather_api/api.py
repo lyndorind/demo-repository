0891c6be31a654f155c99006a0d0c19031b2b10c
xetayac <307517616+xetayac@users.noreply.github.com>
2026-09-11 14:03:10 +0300

    New Code added

--- src/weather_api/api.py
+++ src/weather_api/api.py
@@ -11,3 +11,14 @@ def get_timezone(city: str) -> dict[str, any]:
 
     response = requests.get("https://api.weatherapi.com/v1/timezone.json", params=params)
     return response.json()["location"]["tz_id"]
+
+def get_weather(city: str) -> dict[str, any]:
+    params = {
+        "q": city,
+        "key": WEATHER_API_KEY
+    }
+
+    data = None
+
+    response = requests.get("https://api.weatherapi.com/v1/current.json", params=params)
+    return response.json()["current"]
